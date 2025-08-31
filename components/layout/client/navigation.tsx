471c4b8a080aac950670e1eb01c5e59e72b2fb23
Jan Eris Saludo <janerissaludo@gmail.com>
2025-08-31 22:20:23 +0800

    feat: add sign-in link to navigation when user is not authenticated

--- components/layout/client/navigation.tsx
+++ components/layout/client/navigation.tsx
@@ -4,11 +4,12 @@ import {
   NavigationMenuItem,
   NavigationMenuList,
 } from "@/components/ui/navigation-menu";
+import Link from "next/link";
+import { redirect, useRouter } from "next/navigation";
 import React from "react";
 
 async function Navigation() {
   const session = await auth();
-
   return (
     <nav className="w-full flex items-center justify-between p-4">
       <div>
@@ -24,7 +25,14 @@ async function Navigation() {
             <NavigationMenuItem>About</NavigationMenuItem>
             <NavigationMenuItem>Contact</NavigationMenuItem>
             {!session ? (
-              <NavigationMenuItem className="bg-[#1C1306] px-4 py-1 rounded-xl text-white"></NavigationMenuItem>
+              <NavigationMenuItem className="bg-[#1C1306] px-4 py-1 rounded-xl text-white">
+                <Link
+                  href="/signin"
+                  className="bg-[#1C1306] px-4 py-1 rounded-xl text-white"
+                >
+                  Sign In
+                </Link>
+              </NavigationMenuItem>
             ) : (
               <NavigationMenuItem className="px-4 py-1 rounded-xl font-medium ">
                 {session.user?.name}
