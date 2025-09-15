a330dda971035c62d9bc06c75dfed88d793a1cbe
Jan Eris Saludo <janerissaludo@gmail.com>
2025-09-16 01:48:52 +0800

    fix: edge runtime issue fix version 2

--- auth.ts
+++ auth.ts
@@ -8,32 +8,33 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
   callbacks: {
     ...authConfig.callbacks,
+
+
     async jwt({ token, user }) {
-      if (user) {
-        token.id = user.id
-        token.role = user.role
-        token.customerId = user.customerId
-      } else if (!token.customerId && token.email) {
-        if (token.role === "CUSTOMER") {
-          // Database logic allowed here
-          let customer = await prisma.customer.findUnique({ where: { email: token.email } })
-          if (!customer) {
-            customer = await prisma.customer.create({
-              data: {
-                email: token.email,
-                firstName: "Customer",
-              },
-            })
-          }
-          await prisma.user.updateMany({
-            where: { email: token.email, customerId: null },
-            data: { customerId: customer.id },
-          })
-          token.customerId = customer.id
-        }
-      }
+      // Runs after signIn or on session refresh
+       if (user) {
+    // Ensure customer exists and link it
+    let customer = await prisma.customer.upsert({
+      where: { email: user.email! },
+      update: {},
+      create: {
+        email: user.email!,
+        firstName: user.name,
+      },
+    })
+
+    const dbUser = await prisma.user.update({
+      where: { id: user.id },
+      data: { customerId: customer.id, role: "CUSTOMER" },
+    })
+
+    token.id = dbUser.id
+    token.role = dbUser.role
+    token.customerId = dbUser.customerId
+  }
       return token
     },
+
     async session({ session, token }) {
       if (session.user) {
         session.user.id = token.id as string
