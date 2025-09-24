6ba7abe5dce464988bc254027b49f0db1c53b565
Jan Eris Saludo <janerissaludo@gmail.com>
2025-09-25 01:19:26 +0800

    feat: updated schema to add drinkType and also adjust grid cols to 8

--- components/menu/ProductList.tsx
+++ components/menu/ProductList.tsx
@@ -28,7 +28,7 @@ export function ProductList({
   }
 
   return (
-    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4">
+    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-8 gap-4">
       {products.map((p) => (
         <ProductCard
           key={p.id}
